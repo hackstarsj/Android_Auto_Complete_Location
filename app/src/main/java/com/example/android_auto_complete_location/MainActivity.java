162259596fa683e605b75bb6694634c9370e32d3
sanjeev kumar <sksanjeev362@gmail.com>
2020-03-11 18:39:55 +0530

    Added Lat Lng to Address and Address to Lat Lng Functionality

--- app/src/main/java/com/example/android_auto_complete_location/MainActivity.java
+++ app/src/main/java/com/example/android_auto_complete_location/MainActivity.java
@@ -2,10 +2,22 @@ package com.example.android_auto_complete_location;
 
 import androidx.appcompat.app.AppCompatActivity;
 
+import android.content.Context;
+import android.location.Address;
+import android.location.Geocoder;
 import android.os.Bundle;
+import android.util.Log;
+import android.view.View;
+import android.widget.AdapterView;
 import android.widget.AutoCompleteTextView;
 
 import com.example.android_auto_complete_location.adapter.PlaceAutoSuggestAdapter;
+import com.google.android.gms.maps.model.LatLng;
+
+import java.io.IOException;
+import java.util.ArrayList;
+import java.util.List;
+import java.util.Locale;
 
 public class MainActivity extends AppCompatActivity {
 
@@ -13,7 +25,79 @@ public class MainActivity extends AppCompatActivity {
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
-        AutoCompleteTextView autoCompleteTextView=findViewById(R.id.autocomplete);
+        final AutoCompleteTextView autoCompleteTextView=findViewById(R.id.autocomplete);
         autoCompleteTextView.setAdapter(new PlaceAutoSuggestAdapter(MainActivity.this,android.R.layout.simple_list_item_1));
+
+        autoCompleteTextView.setOnItemClickListener(new AdapterView.OnItemClickListener() {
+            @Override
+            public void onItemClick(AdapterView<?> parent, View view, int position, long id) {
+                Log.d("Address : ",autoCompleteTextView.getText().toString());
+                LatLng latLng=getLatLngFromAddress(autoCompleteTextView.getText().toString());
+                if(latLng!=null) {
+                    Log.d("Lat Lng : ", " " + latLng.latitude + " " + latLng.longitude);
+                    Address address=getAddressFromLatLng(latLng);
+                    if(address!=null) {
+                        Log.d("Address : ", "" + address.toString());
+                        Log.d("Address Line : ",""+address.getAddressLine(0));
+                        Log.d("Phone : ",""+address.getPhone());
+                        Log.d("Pin Code : ",""+address.getPostalCode());
+                        Log.d("Feature : ",""+address.getFeatureName());
+                        Log.d("More : ",""+address.getLocality());
+                    }
+                    else {
+                        Log.d("Adddress","Address Not Found");
+                    }
+                }
+                else {
+                    Log.d("Lat Lng","Lat Lng Not Found");
+                }
+
+            }
+        });
+
     }
+
+    private LatLng getLatLngFromAddress(String address){
+
+        Geocoder geocoder=new Geocoder(MainActivity.this);
+        List<Address> addressList;
+
+        try {
+            addressList = geocoder.getFromLocationName(address, 1);
+            if(addressList!=null){
+                Address singleaddress=addressList.get(0);
+                LatLng latLng=new LatLng(singleaddress.getLatitude(),singleaddress.getLongitude());
+                return latLng;
+            }
+            else{
+                return null;
+            }
+        }
+        catch (Exception e){
+            e.printStackTrace();
+            return null;
+        }
+
+    }
+
+    private Address getAddressFromLatLng(LatLng latLng){
+        Geocoder geocoder=new Geocoder(MainActivity.this);
+        List<Address> addresses;
+        try {
+            addresses = geocoder.getFromLocation(latLng.latitude, latLng.longitude, 5);
+            if(addresses!=null){
+                Address address=addresses.get(0);
+                return address;
+            }
+            else{
+                return null;
+            }
+        }
+        catch (Exception e){
+            e.printStackTrace();
+            return null;
+        }
+
+    }
+
 }
--- app/src/main/java/com/example/android_auto_complete_location/models/PlaceApi.java
+++ app/src/main/java/com/example/android_auto_complete_location/models/PlaceApi.java
@@ -1,5 +1,11 @@
 package com.example.android_auto_complete_location.models;
 
+import android.location.Address;
+import android.location.Geocoder;
+import android.util.Log;
+
+import com.google.android.gms.maps.model.LatLng;
+
 import org.json.JSONArray;
 import org.json.JSONException;
 import org.json.JSONObject;
@@ -10,6 +16,8 @@ import java.net.HttpURLConnection;
 import java.net.MalformedURLException;
 import java.net.URL;
 import java.util.ArrayList;
+import java.util.List;
+import java.util.Locale;
 
 public class PlaceApi {
 
@@ -20,7 +28,7 @@ public class PlaceApi {
         try {
             StringBuilder sb=new StringBuilder("https://maps.googleapis.com/maps/api/place/autocomplete/json?");
             sb.append("input="+input);
-            sb.append("&key=AIzaSyABQvwRUjSdSdTjNjt8TTme6IzUku2sZtA");
+            sb.append("&key=YOUR_API_KEY_HERE");
             URL url=new URL(sb.toString());
             connection=(HttpURLConnection)url.openConnection();
             InputStreamReader inputStreamReader=new InputStreamReader(connection.getInputStream());
@@ -31,6 +39,8 @@ public class PlaceApi {
             while ((read=inputStreamReader.read(buff))!=-1){
                 jsonResult.append(buff,0,read);
             }
+
+            Log.d("JSon",jsonResult.toString());
         }
         catch (MalformedURLException e){
             e.printStackTrace();
@@ -57,4 +67,6 @@ public class PlaceApi {
 
         return arrayList;
     }
+
+
 }
